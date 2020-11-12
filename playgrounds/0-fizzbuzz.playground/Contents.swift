67321e86d14261f659853dd502d6e27e505122bc
alfrch <alfrchdev@gmail.com>
2020-11-12 09:39:53 +0700

    FizzBuzz

---
+++ playgrounds/0-fizzbuzz.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+var numbers = [Int]()
+
+for i in 1...1000 {
+  numbers.append(i)
+}
+
+for num in numbers {
+//  if num % 3 == 0 && num & 5 == 0 {
+  if num % 15 == 0 {
+    print("\(num) fizzbuzz")
+  } else if num % 3 == 0 {
+    print("\(num) fizz")
+  } else if num % 5 == 0 {
+    print("\(num) buzz")
+  } else {
+    print(num)
+  }
+}
