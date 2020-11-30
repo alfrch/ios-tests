ec1fbfd462d80fb09ea3d481cd3d27b26f9ac791
alfrch <alfrchdev@gmail.com>
2020-11-30 09:51:07 +0700

    update 7.4.challenges

--- swift-apprentice/7.4.Challenges.playground/Contents.swift
+++ swift-apprentice/7.4.Challenges.playground/Contents.swift
@@ -52,7 +52,7 @@ for item in array5 { print(item) }
  2. Write a function that removes the first occurrence of a given integer from an array of integers. This is the signature of the function:
  -----------------------------------------------------------*/
 
-let array6 = [1, 2, 3, 4, 1, 2, 2, 4, 5]
+let array6 = [1,2, 3, 4, 1, 2, 2, 4, 5]
 
 func removingOnce(_ item: Int, from array: [Int]) -> [Int] {
 //  var newArray: [Int] = array // solution 1 and 2
@@ -93,7 +93,7 @@ func removingOnce(_ item: Int, from array: [Int]) -> [Int] {
   return newArray
 }
 
-print("2. result: ", removingOnce(2, from: array6))
+print("2. ", removingOnce(2, from: array6))
 
 
 
@@ -114,7 +114,7 @@ func removing(_ item: Int, from array: [Int]) -> [Int] {
   return newArray
 }
 
-print("3. result: \(removing(2, from: array6))")
+print("3. \(removing(2, from: array6))")
 
 
 
@@ -136,7 +136,7 @@ func reversed(_ array: [Int]) -> [Int] {
   return reverseArray
 }
 
-print(reversed(array7))
+print("4. \(reversed(array7))")
 
 
 
@@ -163,4 +163,99 @@ func middle(_ array: [Int]) -> Int? {
   return nil
 }
 
-print(middle(array8))
+print("5. \(middle(array8))")
+
+
+
+
+/*-----------------------------------------------------------
+ 6. Write a function that calculates the minimum and maximum value in an array of integers. Calculate these values yourself; don’t use the methods min and max. Return nil if the given array is empty.
+ -----------------------------------------------------------*/
+
+//func minMax(of numbers: [Int]) -> (min: Int, max: Int)? {
+//
+//}
+
+
+
+
+/*-----------------------------------------------------------
+ 7. Which of the following are valid statements?
+ -----------------------------------------------------------*/
+
+//let dict1: [Int, Int] = [:] // not valid
+
+//let dict2 = [:] // not valid -> Empty collection literal requires an explicit type
+
+let dict3: [Int: Int] = [:] // valid
+
+let dict4 = ["One": 1, "Two": 2, "Three": 3]
+
+//dict4[1]
+
+dict4["One"]
+
+//dict4["Zero"] = 0 // Cannot assign through subscript: 'dict4' is a 'let' constant
+
+//dict4[0] = "Zero"
+
+var dict5 = ["NY": "New York", "CA": "California"]
+
+dict5["NY"]
+
+dict5["WA"] = "Washington"
+
+dict5["CA"] = nil
+
+
+
+/*-----------------------------------------------------------
+ 8. Given a dictionary with two-letter state codes as keys, and the full state names as values, write a function that prints all the states with names longer than eight characters. For example, for the dictionary ["NY": "New York", "CA": "California"], the output would be California.
+ -----------------------------------------------------------*/
+
+var dict6 = ["NY": "New York", "CA": "California"]
+
+func stateName(_ dictionary: [String: String]) -> String {
+  
+  for (_, state) in dictionary {
+    if Array(state).count > 8 {
+      print(state)
+    }
+  }
+  
+  return ""
+}
+
+print("8. \(stateName(dict6))")
+
+
+
+/*-----------------------------------------------------------
+ 9. Write a function that combines two dictionaries into one. If a certain key appears in both dictionaries, ignore the pair from the first dictionary.
+ -----------------------------------------------------------*/
+
+func merging(_ dict1: [String: String], with dict2: [String: String]) -> [String: String] {
+  
+  return [String: String]()
+}
+
+
+
+/*-----------------------------------------------------------
+ 10. Declare a function occurrencesOfCharacters that calculates which characters occur in a string, as well as how often each of these characters occur. Return the result as a dictionary. This is the function signature:
+ -----------------------------------------------------------*/
+
+let str = "abcdefgaag"
+
+func occurencesOfCharacters(in text: String) -> [Character: Int] {
+  var dict: [Character: Int] = [:]
+  
+  for i in Array(text) {
+    dict[i, default: 0] += 1
+  }
+  
+  return dict
+}
+
+print("10. \(occurencesOfCharacters(in: str))")
+
